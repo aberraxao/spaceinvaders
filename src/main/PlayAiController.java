14abbbba5aafffcf9f2bdea3229fe33fa3869d4f
aberraxao <diana_milu@live.com.pt>
2023-05-13 19:02:12 +0100

    AiControler removal

--- src/main/PlayAiController.java
+++ src/main/PlayAiController.java
@@ -1,12 +1,6 @@
 package main;
 
-import controllers.AiController;
-import controllers.GameController;
-import controllers.RandomController;
-import space.Board;
 import space.SpaceInvaders;
-
-import java.util.Random;
 
 public class PlayAiController {
 	public static void main(String[] args) {
